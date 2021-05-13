d991bfd8bc34aa6df1687216342e650392eb0586
Jeff LaFay <jeff@presence.io>
2021-05-13 10:15:12 -0400

    Section 3 - string reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversedStr = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        const charToAppend = str[i];
+        reversedStr += charToAppend;
+    }
+
+    return reversedStr;
+}
+
+function reverseInstructorSolutionOne(str) {
+    return str
+        .split('')
+        .reverse()
+        .join('');
+}
+
+function reverseInstructorSolutionTwo(str) {
+    let reversed = '';
+
+    for (const character of str) {
+        reversed = character + reversed;
+    }
+
+    return reversed;
+}
+
+function reverseInstructorSolutionThree(str) {
+    return str
+        .split('')
+        .reduce((reversed, character) => character + reversed, '');
+}
 
 module.exports = reverse;

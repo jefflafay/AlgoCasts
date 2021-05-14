3896440c53a96c66b1958abf9b66a2cf635c64cc
Jeff LaFay <jeff@presence.io>
2021-05-13 21:01:28 -0400

    Section 7 - fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,43 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    const numbers = Array(n).fill(0).map((_, i) => i + 1);
+
+    for (let num of numbers) {
+        const isMultipleOfThree = num % 3 === 0;
+        const isMultipleOfFive = num % 5 === 0;
+
+        let answer = '';
+
+        if (isMultipleOfThree) { 
+            answer += 'fizz';
+        }
+
+        if (isMultipleOfFive) {
+            answer += 'buzz'
+        }
+
+        if (answer == '') {
+            console.log(num);
+        } else {
+            console.log(answer);
+        }
+    }
+}
+
+function fizzBuzzInstructorSolution(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+}
 
 module.exports = fizzBuzz;

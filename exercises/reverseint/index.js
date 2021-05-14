fd4660a60bbc7f618fc841c94672936563b6231f
Jeff LaFay <jeff@presence.io>
2021-05-13 20:19:32 -0400

    Section 5 - Reverse Ints

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const numStr = n.toString();
+    const reversedNumStr = numStr
+        .split('')
+        .reverse()
+        .join('')
+        .replace('-', '');
+
+    return Math.sign(n) == -1 ? parseInt(reversedNumStr) * -1 : parseInt(reversedNumStr);
+}
+
+function reverseIntInstructorSolution(n) {
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join();
+
+    return parseInt(reversed) * Math.sign(n);
+}
+
 
 module.exports = reverseInt;

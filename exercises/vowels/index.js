eb88716b742fa59ee40eb48901a9867358953dc6
Jeff LaFay <jeff@presence.io>
2021-05-15 15:40:02 -0400

    Section 13 - vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,41 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = str
+        .toLowerCase()
+        .replace(/[^aeiou]/g, '');
+
+    return vowels.length;
+}
+
+function vowelsMyIterativeSolution(str) {
+    let vowelCount = 0;
+
+    for (let character of str.toLowerCase()) {
+        if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u') {
+            vowelCount++;
+        }
+    }
+
+    return vowelCount;
+}
+
+function instructorIterativeSolution(str) {
+    let count = 0;
+
+    for (let char of str.toLowerCase()) {
+        if (['a','e','i','u'].includes(char)) {
+            count++
+        }
+    }
+
+    return count;
+}
+
+function instructorSecondSolution(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;

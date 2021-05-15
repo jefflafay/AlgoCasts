6ecd6bd13e4d96b35984d3c47dd558b068842aa9
Jeff LaFay <jeff@presence.io>
2021-05-15 14:02:43 -0400

    Section 11 - steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let i = 0; i < n; i++) {
+        console.log('#'.repeat(i + 1).padEnd(n));
+    }
+}
+
+function stepsInstructorSolutionOne(n) {
+    for (let row = 0; row < n; row++) {
+        let step = '';
+
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += '#';
+            } else {
+                stair += ' ';
+            }
+        }
+
+        console.log(step);
+    }
+}
+
+function stepsInstructorSolutionTwo(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
+    }
+
+    // we're at the end of the row
+    if (n === stair.length) {
+        console.log(stair);
+        stepsInstructorSolutionTwo(n, row + 1);
+        return;
+    }
+
+    // still building the row
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    stepsInstructorSolutionTwo(n, row, stair);
+}
 
 module.exports = steps;

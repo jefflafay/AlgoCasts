462d19b4bdc7581eda8c04e288903270a9d4e07d
Jeff LaFay <jeff@presence.io>
2021-05-15 15:26:01 -0400

    Section 12 - pyrmaids

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,67 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+    const midPoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+        
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (midPoint - row <= column && midPoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+
+        console.log(level);
+    }
+}
+
+pyramid(3);
+
+function pyramidInstructorIterative(n) {
+    const midPoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+        
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (midPoint - row <= column && midPoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+
+        console.log(level);
+    }
+}
+
+function pyramidInstructorRecursive(n, row = 0, level = '') {
+    const columnCount = 2 * n - 1;
+
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === columnCount) {
+        console.log(level);
+        pyramidInstructorRecursive(n, row + 1);
+        return;
+    }
+
+    const midpoint = Math.floor(columnCount / 2);
+    let add;
+
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+
+    pyramidInstructorRecursive(n, row, level + add);
+}
 
 module.exports = pyramid;
